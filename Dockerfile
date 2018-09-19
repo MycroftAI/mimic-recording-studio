a91be44bac3c10c255014efb8c2aa92227ce0134
Michael Nguyen <ppnguyen91@gmail.com>
2018-09-19 15:32:30 -0500

    backend init

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+# TODO
+FROM ubuntu
---
+++ backend/app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask, request, send_file, jsonify, make_response, Response
+from flask.views import MethodView
+from flask_cors import CORS
+
+
+# class Data(MethodView):
+
+#     def get()
